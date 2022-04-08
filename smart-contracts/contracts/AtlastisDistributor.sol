5eef8c50618a082e3bd9c4c93052928278e5b780
Asuzu Kosisochukwu <34872572+asuzukosi@users.noreply.github.com>
2022-04-08 08:20:22 +0100

    Update AtlastisDistributor.sol

--- smart-contracts/contracts/AtlastisDistributor.sol
+++ smart-contracts/contracts/AtlastisDistributor.sol
@@ -9,33 +9,45 @@ interface IAtlantisToken is IERC20 {
 
 interface ServiceInterface {
   // This is an interface to define a NestCoin service
+  function getPrice() external returns(uint256);
+  function addUser(address account) external;
 }
 
+
 contract AtlantisDistributor {
-    // This is a smart contract for performing the batch distribution of the Atlantis tokens by the admins
+    // Thsi is a smart contract for performing the batch distribution of the Atlantis tokens by the admins
 
     mapping(address=>bool)admins;
-    mapping(address => bool) receivers;
+    address[] recievers;
 
+    address public atlantisToken;
     uint256 public totalDistributed = 0;
-    uint256 public totalReceivers = 0;
+    uint256 public totalRecievers = 0;
 
-    IAtlantisToken atlantisToken;
-
-    event DistributionComplete(uint256 numberOfReceivers, uint256 amount);
+    event DistributionComplete(uint256 numberOfRecievers, uint256 amount);
 
     modifier isAdmin() {
+        require(admins[msg.sender] == true);
         _;
     }
 
+    struct Distribution {
+        address admin;
+        uint256 time;
+        uint256 numRecievers;
+        uint256 amountDisbursed;
+    }
+
+    Distribution[] distributions;
+
     constructor(address _atlantisToken) {
-        atlantisToken = IAtlantisToken(_atlantisToken);
+        atlantisToken = _atlantisToken;
         admins[msg.sender] = true;
     }
     
     // add admin to the list of admins
     function addAdmin(address newAdmin_) public isAdmin {
-        
+        admins[newAdmin_] = true;
     }
 
     // check if address is an admin
@@ -44,28 +56,51 @@ contract AtlantisDistributor {
     }
 
     // distribute a certain amount of tokens to a list of addresses
-    function distributeToken(address[] memory _addresses, uint256[] memory _amounts) public isAdmin {
+    function distributeToken(address[] memory _addresses, uint256 _amount) public isAdmin {
         require(_addresses.length <= 200, "can not distribute to more than 200 at once");
 
-        uint256 totalAmount;
-
         for(uint256 i=0; i < _addresses.length; i++) {
-            atlantisToken.mint(_addresses[i], _amounts[i]);
-
-            if(receivers[_addresses[i]] != true) {
-                totalReceivers = totalReceivers + 1;
-                receivers[_addresses[i]] == true;
-            }
-
-            totalAmount += _amounts[i];
-            totalDistributed = totalDistributed + _amounts[i];
+            IERC20(atlantisToken).mint(_addresses[i], _amount);
+            addReciever(_addresses[i]);
+            totalDistributed = totalDistributed + _amount;
+            totalRecievers = totalRecievers + 1;
         }
-
-        emit DistributionComplete(_addresses.length, totalAmount);
+        Distribution memory distribution = Distribution({admin: msg.sender, time: block.timestamp, numRecievers: _addresses.length, amountDisbursed: _addresses.length * _amount});
+        distributions.push(distribution);
+        emit DistributionComplete(_addresses.length, _amount);
+    }
+    // add a reciever to the list of recievers if the address is not already in the recievers list
+    function addReciever(address reciever) private {
+        for (uint256 i=0; i < recievers.length; i++){
+            if(reciever == recievers[i]){
+                return;
+            }
+        }
+        recievers.push(reciever);
     }
 
-    // check if an address is a receiver, ie has been distributed tokens from this contract
-    function isReceiver(address _receiver) public view returns(bool){
-        return receivers[_receiver];
+    // check if an address is a reciever, ie has been distributed tokens from this contract
+    function isReciever(address reciever) public view returns(bool){
+        for (uint256 i=0; i < recievers.length; i++){
+            if(reciever == recievers[i]){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // Get the balance of an address that was distributed tokens from this distributor
+    function balanceOfReciever(address reciever) public view returns(uint256){
+        require(isReciever(reciever), "address is not a reciever from this distributor");
+        return IERC20(atlantisToken).balanceOf(reciever);
+    }
+
+    // allows a user to subscribe for a NestCoin service
+    function subscribeToService(address account, address service) public {
+       // get the price of the service
+       uint256 servicePrice = ServiceInterface(service).getPrice();
+       IERC20(atlantisToken).transferFrom(account, service, servicePrice);
+       ServiceInterface(service).addUser(account);
     }
 }
